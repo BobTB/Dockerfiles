8b02abe525b2807ede22cf0f2fcd85f1265876a9
Dryusdan <contact@dryusdan.fr>
2017-06-08 11:35:26 +0200

    active send mail

--- presentator/rootfs/presentator/common/config/main-local.php
+++ presentator/rootfs/presentator/common/config/main-local.php
@@ -11,7 +11,7 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-            'useFileTransport' => true,
+            'useFileTransport' => false,
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.

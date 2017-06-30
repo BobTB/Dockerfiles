c26e41f656f48e88cba4a0e594e7bf226ef9dbf9
Dryusdan <contact@dryusdan.fr>
2017-06-30 15:11:23 +0200

    remove useless echo && build image

--- peertube/builder.sh
+++ peertube/builder.sh
@@ -6,7 +6,6 @@ LAST_COMMIT_ID=$(git log -n 1 --pretty=format:%H --)
 if [ "$COMMIT_ID" == "$LAST_COMMIT_ID" ]; then
    echo "build image"
    docker build -t registry.dryusdan.fr/dryusdan/peertube peertube/
-   echo "push image"
    docker push registry.dryusdan.fr/dryusdan/peertube
    docker rmi registry.dryusdan.fr/dryusdan/peertube
 else
--- standardfile/builder.sh
+++ standardfile/builder.sh
@@ -6,7 +6,6 @@ LAST_COMMIT_ID=$(git log -n 1 --pretty=format:%H --)
 if [ "$COMMIT_ID" == "$LAST_COMMIT_ID" ]; then
    echo "build image"
    docker build -t registry.dryusdan.fr/dryusdan/etherpad standarfile/
-   echo "push image"
    docker push registry.dryusdan.fr/dryusdan/etherpad
    docker rmi registry.dryusdan.fr/dryusdan/etherpad
 else

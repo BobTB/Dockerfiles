34464b4630a4c68941300790ebf8676984b4a07f
Dryusdan <contact@dryusdan.fr>
2017-09-11 13:50:44 +0200

    update builder

--- ghost/builder.sh
+++ ghost/builder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMMIT_ID=$(git log -n 1 --pretty=format:%H -- shaarli/)
+COMMIT_ID=$(git log -n 1 --pretty=format:%H -- ghost/)
 LAST_COMMIT_ID=$(git log -n 1 --pretty=format:%H --)
 
 if [ -n "$1" ]; then
@@ -11,9 +11,9 @@ fi
 
 if [ "$COMMIT_ID" == "$LAST_COMMIT_ID" ]  || [ "$FORCE_BUILD" -eq "1" ]; then
    echo "build image"
-   docker build --no-cache -t registry.dryusdan.fr/dryusdan/shaarli shaarli/
-   docker push registry.dryusdan.fr/dryusdan/shaarli
-   docker rmi registry.dryusdan.fr/dryusdan/shaarli
+   docker build --no-cache -t registry.dryusdan.fr/dryusdan/ghost ghost/
+   docker push registry.dryusdan.fr/dryusdan/ghost
+   docker rmi registry.dryusdan.fr/dryusdan/ghost
 else
    echo "image is recent. No build"
 fi

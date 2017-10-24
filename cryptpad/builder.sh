567b94c8cad862d10e125b401da9eb5624573e1a
Dryusdan <dryusdan@git@drycat.fr>
2017-10-24 12:14:41 +0200

    Ajouter 'cryptpad/builder.sh'

---
+++ cryptpad/builder.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git clone --branch 1.17.0 https://github.com/xwiki-labs/cryptpad.git
+docker build -t registry.dryusdan.fr/dryusdan/cryptpad ./cryptpad/Dockerfile
+docker push registry.dryusdan.fr/dryusdan/cryptpad
+docker rmi registry.dryusdan.fr/dryusdan/cryptpad
+rm -rf cryptpad

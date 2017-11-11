0fc85376b3cd6be6fce86fd7bd85f1b0d63ed63f
KFM\jaroadmin <jf@kfm.si>
2017-11-11 12:43:14 +0100

    Fix qew qweqwasdasdasd  qweqwe

--- cryptpad/builder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git clone --branch 1.18.0 https://github.com/xwiki-labs/cryptpad.git
-docker build -t registry.dryusdan.fr/dryusdan/cryptpad ./cryptpad/Dockerfile
-docker push registry.dryusdan.fr/dryusdan/cryptpad
-docker rmi registry.dryusdan.fr/dryusdan/cryptpad
-rm -rf cryptpad
